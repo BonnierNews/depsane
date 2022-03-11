117c930b438ae31731d4cb2d3248f70093b8b153
Markus Ekholm <markus.ekholm@bonniernews.se>
2022-03-11 10:26:51 +0100

    bump dpes

--- bin/depsane.js
+++ bin/depsane.js
@@ -6,6 +6,6 @@ const main = require("../");
 
 (async () => {
   const retCode = await main(minimist(process.argv.slice(2)));
-  // eslint-disable-next-line no-process-exit
+  // eslint-disable-next-line node/no-process-exit
   process.exit(retCode);
 })();
